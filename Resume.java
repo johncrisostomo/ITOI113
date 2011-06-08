43e0f42a82b5f8c6b48447b97a1035c8ed91b1e2
John Crisostomo <jdcrisostomo@gmail.com>
2011-06-08 18:44:06 +0800

    initial commit

---
+++ Resume.java
@@ -0,0 +1,24 @@
+public class Resume {
+    public static void main(String args[]) {
+	System.out.print("\n\n");
+	System.out.println("2281 - A Santiago        |  [02]6595578");
+	System.out.println("corner Sanchez Streets   |  09166889281");
+	System.out.println("Baclaran, Paranaque City |  jscrisostomo@feu-eac.edu.ph");
+	System.out.println("\nJohn Dominic S. Crisostomo\n");
+	System.out.println("Objective : \n");
+	System.out.println("     To work in a team oriented environment and put into practice the theories of web development that we have learned in school.\n");
+	System.out.println("Work Experience : \n");
+	System.out.println("January 2011 - PRESENT       Summit Media");
+	System.out.println("                             Contributor\n");
+	System.out.println("July 2008 - January 2009     TeleTech");
+	System.out.println("                             Technical Support Associate\n");
+	System.out.println("Educational Background : \n");
+	System.out.println("2005 - 2007                  University of Santo Tomas");
+	System.out.println("                             Bachelor of Arts in Literature");
+	System.out.println("                             Undergraduate\n");
+	System.out.println("2010 - PRESENT               FEU - East Asia College");
+	System.out.println("                             Bachelor of Arts in Information Technology with specialization");
+	System.out.println("                                in Web Applications Development");
+	System.out.println("                             Undergraduate\n");
+    }
+}
