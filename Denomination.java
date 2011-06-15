627c60bafbd96ae09a478916a2eb78d69f57e8ab
John Crisostomo <jdcrisostomo@gmail.com>
2011-06-15 12:21:57 +0800

    finished denomination, used greedy algorithm

---
+++ Denomination.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Denomination {
+    public static void main(String args[]) throws Exception  {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	int amount, sum = 0, counter = 0;
+	int[] denominations = {1000, 500, 200, 100, 50, 20, 10, 5, 1};
+	int[] answer = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+	System.out.print("Please enter the amount : ");
+	amount = Integer.parseInt(br.readLine());
+	
+	while(sum != amount) {
+	    if((denominations[counter]+sum)<= amount){
+		answer[counter]++;
+		sum = sum + denominations[counter];
+	    } else {
+		 counter++;
+	    }
+	}
+	for(int i = 0; i < answer.length; i++) {
+	    System.out.printf("%4d : %d\n", denominations[i], answer[i]);
+	}
+    }
+}
