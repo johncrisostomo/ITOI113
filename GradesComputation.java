cb9ae7321b5df73c80ec571abc83334d4c3db47e
John Crisostomo <jdcrisostomo@gmail.com>
2011-06-17 10:49:36 +0800

    finished coding GradesComputation.java, unsure of formula

--- GradesComputation.java
+++ GradesComputation.java
@@ -10,8 +10,28 @@
 // cs = 40% lq + 30% sq + 25% r + 5% te
 
 import java.io.*;
+import java.text.*;
 
 public class GradesComputation {
     public static void main(String args[]) throws Exception {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	DecimalFormat formatter = new DecimalFormat("00.00");
+	float sq, lq, r, te, me;
+	float cs, mg;
+	System.out.print("Enter grade for Short Quiz : ");
+	sq = Float.parseFloat(br.readLine());
+	System.out.print("Enter grade for Long Quiz : ");
+	lq = Float.parseFloat(br.readLine());
+	System.out.print("Enter grade for Recitation : ");
+	r = Float.parseFloat(br.readLine());
+	System.out.print("Enter grade for Teacher's Evaluation : ");
+	te = Float.parseFloat(br.readLine());
+	System.out.print("Enter grade for Midterm Exam : " );
+	me = Float.parseFloat(br.readLine());
+	
+	cs = (lq * .40) + (sq * .30) + (r * .25) + (te * .05);
+	mg = (cs * .60) + (me * .40);
+
+	System.out.println("The grade is " + formatter.format(mg));
     }
 }
