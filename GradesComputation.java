a44a8550f37b200d51715d434101b29da85850b4
John Crisostomo <jdcrisostomo@gmail.com>
2011-06-17 10:21:07 +0800

    started to work in Gradescomputation.java

---
+++ GradesComputation.java
@@ -0,0 +1,17 @@
+/*
+ * Created by Crisostomo, John Dominic S. (2007 - 10993)
+ *   on June 17, 2011 ( 10:17am)
+ *
+ *  E-mail : jscrisostomo@feu-eac.edu.ph
+ */
+
+// formula :  
+// mg = 60% cs + me
+// cs = 40% lq + 30% sq + 25% r + 5% te
+
+import java.io.*;
+
+public class GradesComputation {
+    public static void main(String args[]) throws Exception {
+    }
+}
