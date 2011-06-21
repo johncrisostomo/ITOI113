34be9a0d1c929e598c45b6599088797d1d6854a8
John Crisostomo <arsc4riosus@gmail.com>
2011-06-21 22:55:12 +0800

    finished coding the prompts and the inputs

--- CrisostomoLab3A/src/crisostomolab3a/SimplePayslip.java
+++ CrisostomoLab3A/src/crisostomolab3a/SimplePayslip.java
@@ -3,6 +3,9 @@
  * and open the template in the editor.
  */
 package crisostomolab3a;
+import java.io.*;
+import java.util.*;
+import java.text.*;
 
 /**
  *
@@ -13,7 +16,28 @@ public class SimplePayslip {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws Exception {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        DecimalFormat formatter = new DecimalFormat("##,##0.00");
+        Date today = new Date();
+        
+        // variable declaration
+        int number;
+        String name;
+        double salary;
+        double ot;
+        
+        // prompts and inputs
+        System.out.println("Employee Payslip");
+        System.out.print("Enter Employee Number : ");
+        number = Integer.parseInt(input.readLine());
+        System.out.print("Enter Employee Name : ");
+        name = input.readLine();
+        System.out.print("Enter Employee's Basic Salary : ");
+        salary = Double.parseDouble(input.readLine());
+        System.out.print("Enter Employee's OT Hours : ");
+        ot = Double.parseDouble(input.readLine()); 
+        
+        // prints payslip
     }
 }
