a68d80ee6793ec6acf7f3566717f495f95f0b1ab
John Crisostomo <jdcrisostomo@gmail.com>
2011-07-03 17:12:05 +0800

    finished with Lab4A

--- CrisostomoLab4A/src/crisostomolab4a/NumberAndCharacterSorting.java
+++ CrisostomoLab4A/src/crisostomolab4a/NumberAndCharacterSorting.java
@@ -4,7 +4,7 @@
  */
 
 package crisostomolab4a;
-
+import java.io.*;
 /**
  *
  * @author arscariosus
@@ -14,8 +14,36 @@ public class NumberAndCharacterSorting {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws Exception {
+        	BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+	String uinput;
+	int i, j;
+	char tmp;
+
+
+	System.out.print("Enter input : ");
+	uinput = input.readLine();
+
+	char inputchar[] = new char[uinput.length()];
+
+	for(int x = 0; x < uinput.length(); x++) {
+	    inputchar[x] = uinput.charAt(x);
+	}
+
+	for(i = 0; i < uinput.length()-1; i++) {
+	    for(j = i+1;j < uinput.length(); j++) {
+		if(inputchar[j] < inputchar[j-1]) {
+			tmp = inputchar[j];
+			inputchar[j] = inputchar[j-1];
+			inputchar[j-1] =tmp;
+		}
+	    }
+	}
+
+	for(int x = uinput.length()-1; x >= 0; x--){
+	    System.out.print(inputchar[x]);
+	}
+	System.out.println();
     }
 
 }
