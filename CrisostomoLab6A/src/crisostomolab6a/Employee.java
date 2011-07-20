ea785a163e4520a2819ce4fabb494c08280dfa4f
John Crisostomo <jdcrisostomo@gmail.com>
2011-07-20 19:37:55 +0800

    started Lab6A

---
+++ CrisostomoLab6A/src/crisostomolab6a/Employee.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package crisostomolab6a;
+
+/**
+ *
+ * @author arscariosus
+ */
+public class Employee {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
---
+++ CrisostomoLab6A/src/crisostomolab6a/SimplePaySlip.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package crisostomolab6a;
+
+/**
+ *
+ * @author arscariosus
+ */
+public class SimplePaySlip {
+    private int employeeNo;
+    private String employeeName;
+    private String position;
+    private String payGrade;
+    private double basic;
+    private int otHours;
+    private double grossPay;
+    
+
+}
